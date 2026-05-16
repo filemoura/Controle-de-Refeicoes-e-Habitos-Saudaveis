31f043fd0250f2d9ed7bcd7f3ff71b6268d43c17
filemoura <filemoura@sempreceub.com>
2026-05-16 12:03:13 -0300

    feat: integração com API pública e teste

---
+++ src/test/java/br/com/bootcamp/ApiDicaSaudavelTest.java
@@ -0,0 +1,15 @@
+package br.com.bootcamp;
+
+public class ApiDicaSaudavelTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Teste da API iniciado...");
+
+        ApiDicaSaudavel.buscarDica();
+
+        System.out.println("Teste finalizado com sucesso!");
+
+    }
+
+}
